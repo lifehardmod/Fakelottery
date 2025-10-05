9a64428ecc49e077f17314871a70e0902c1951c8
서주원 <kciimnida@gmail.com>
2025-10-06 04:10:36 +0900

    feat: analystics추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
+import { Analytics } from "@vercel/analytics/next";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <BrowserRouter>
       <App />
     </BrowserRouter>
